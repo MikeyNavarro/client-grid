6e89ec400b6bdd4e3ae2afa9d1090f8a5bc43997
Mikey Navarro <mikey.navarro.135@gmail.com>
2022-12-30 13:39:12 -0800

    css grid

---
+++ src/components/Logo.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Logo = () => {
+  return (
+    <div className="logo">
+      <div className="items">Logo</div>
+      <div className="items">Business Name</div>
+      <div className="items">Est. 2020</div>
+    </div>
+  );
+};
+
+export default Logo;
